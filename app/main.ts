eb8f979606913085c03d4ab85df4fdda525072fd
Nathan Walker <walkerrunpdx@gmail.com>
2016-05-19 14:08:23 -0700

    setup

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import {nativeScriptBootstrap} from "nativescript-angular/application";
+require('nativescript-websockets');
 import {AppComponent} from "./app.component";
 
 nativeScriptBootstrap(AppComponent);
