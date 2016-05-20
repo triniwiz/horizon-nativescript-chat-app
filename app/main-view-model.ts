5b06bf5df1953a8b02db868b2357be01d4aff68c
Nathan Walker <walkerrunpdx@gmail.com>
2016-05-19 18:33:43 -0700

    feat: vanilla setup

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,38 +1,60 @@
-import observable = require("data/observable");
+var Horizon = require('@horizon/client/dist/horizon-dev');
+import {Observable} from 'data/observable';
+import {ObservableArray} from 'data/observable-array';
+var config = require('./config');
 
-export class HelloWorldModel extends observable.Observable {
+const SERVER_URL = config.SERVER_URL;
 
-    private _counter: number;
-    private _message: string;
-
-    get message(): string {
-        return this._message;
-    }
-    set message(value: string) {
-        if (this._message !== value) {
-            this._message = value;
-            this.notifyPropertyChange("message", value)
-        }
-    }
+export class HorizonDemo extends Observable {
+    public messages: ObservableArray<any>;
+    public newMessage: string;
+    private horizon;
+    private chat;
+    private avatar_url = `http://api.adorable.io/avatars/50/${new Date().getMilliseconds()}.png`;
 
     constructor() {
         super();
+        this.horizon = new Horizon({ host: SERVER_URL });
 
-        // Initialize default values.
-        this._counter = 42;
-        this.updateMessage();
+        this.horizon.onReady()
+            .subscribe(status => { console.log(status.type) })
+
+        this.horizon.onDisconnected()
+            .subscribe(status => { console.log(status.type) })
+
+        this.horizon.onSocketError()
+            .subscribe(status => { console.log(status.type) })
+
+        this.chat = this.horizon('messages');
+
+        this.getChats().subscribe((newMessage: any) => {
+            this.messages.push(newMessage);
+        });
     }
 
-    private updateMessage() {
-        if (this._counter <= 0) {
-            this.message = "Hoorraaay! You unlocked the NativeScript clicker achievement!";
-        } else {
-            this.message = this._counter + " taps left";
-        }
+    private getChats() {
+        return this.chat
+            .order('timeStamp', 'descending')
+            .limit(10)
+            .watch();
     }
-
-    public onTap() {
-        this._counter--;
-        this.updateMessage();
+    
+    public addMessage() {
+        this.chat
+            .store({
+                text: this.newMessage,
+                timeStamp: new Date(),
+                avatar: this.avatar_url,
+            }).subscribe((res) => {
+                console.log(res);
+                console.log(`Adding new message:`);
+                console.log(this.newMessage);
+            },
+            (error) => { console.log(error) });
+        this.set('newMessage', '');
+    }
+    
+    private getStatus() {
+        return this.horizon.status();
     }
 }
